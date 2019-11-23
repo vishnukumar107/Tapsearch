5e035d2b3650b5354403ca52baa3deaa2bfbcff1
vishnu kumar <vishnukumar@vishnus-MacBook-Air.local>
2019-11-23 12:27:25 +0530

    third commit

---
+++ README.md
@@ -0,0 +1,15 @@
+
+Once the webpage of the tapsearch is opened you will see 
+        -a form to enter the documents
+        -a searchbar to search for a word
+        -a button to clear the index 
+
+-->First,step is to enter the documents and click index which
+   will make the documents converted into a inverted index
+
+-->Then, you can search for any word present in the documents.
+    It will display the top 10 paragraphs having most words.
+
+-->Finally,If you want to add some other documents, click the 
+    button to clear the indexed documents and then we can add
+    a new one.
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 var dict = {};
 var output=[];
+//functions to clean the unwanted characters only for the processing
 function clean(l){
     var cleanstr=" ";
     for (let x=0;x<l.length;x++){
@@ -10,9 +11,9 @@ function clean(l){
             cleanstr+=l[x];
         }
     }
-    //console.log(cleanstr)
-    return cleanstr
+    return cleanstr;
 }
+//function to store and index the input given
 function store() {
     var myform =document.getElementById("userform").value;
     if(myform=="")
@@ -22,7 +23,6 @@ function store() {
     document.getElementById('display').innerHTML = display;
     output = myform.split("\n\n")
     var ln=0;
-    var doc=0;
     for(let i=0;i<output.length;i++){
         ln+=1;
         let cleanout=clean(output[i]);
@@ -30,62 +30,60 @@ function store() {
         let l=cleanout.split(" ");
         l.forEach(word => {
            if(!dict.hasOwnProperty(word)){
-            dict[word]=[]
-            dict[word].push(1)
-            dict[word].push([ln])
+            dict[word]=[];
+            dict[word].push(1);
+            dict[word].push([ln]);
             } 
             else{
-                dict[word][0]+=1
-                dict[word][1].push(ln)
+                dict[word][0]+=1;
+                dict[word][1].push(ln);
             }
         });
     }
     for (let [key, value] of Object.entries(dict)) {
-        //console.log(key);
         value[1]=arrange(value[1]);
-        console.log(value[1]);
-        value[1]=new Set(value[1])
-        //console.log(sample)
+        value[1]=new Set(value[1]);
       }
 }
+//function to display the result
 function displayresult(){
     var word =document.getElementById("userword").value;
     var z=0;
     resultstr=`<h1 align="center">The Entered Word '${word}' is present in </h1>`;
     if(dict.hasOwnProperty(word)){
-        let setvalue=dict[word][1]
+        let setvalue=dict[word][1];
         for (const k of setvalue.keys()) {
-            z+=1
+            z+=1;
             if(z<=10){
             resultstr+=`
                     <h4>${output[k-1]}</h4>
             `
-            //console.log(output[k-1])
             }
           }
-        //console.log(resultstr)
-        highlight_word(resultstr,word)
+        highlight_word(resultstr,word);
     }
     else{
-        resultstr=`<h4>The Entered Word '${word}' is not present in the document</h4>`
+        resultstr=`<h4>The Entered Word '${word}' is not present in the document</h4>`;
         document.getElementById('resultpara').innerHTML=resultstr;
     }
 }
+//function to highlight the word in the result
 function highlight_word(searchpara,text)
 {
-text=text+" ";
  if(text)
  {
   var pattern=new RegExp("(" + text + ")", "gi");
   var new_text=searchpara.replace(pattern, "<span class='highlight'>" + text + "</span>");
   document.getElementById('resultpara').innerHTML=new_text;
  }
+ //function to clear the page
 }
 function clearpara(){
     document.getElementById('userform').value="";
     document.getElementById('userword').value="";
     location.reload();
 }
+//function to arrange the paragraph based most words present
 function arrange(myArray)
 {
     var newArray = [];
